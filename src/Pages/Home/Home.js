7a59e1690662016a6e1fa753774a4a1b07660658
sakshi006 <saks5011@gmail.com>
2022-05-21 14:24:19 +0530

    feat/home page

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Home.css";
+
+const Home = () => {
+  const [user, setUser] = useState("");
+
+  const handleChange = (e) => {
+    setUser(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      localStorage.setItem("name", user);
+      window.location.reload(false);
+    }
+  };
+
+  return (
+    <div className="home-container">
+      <div className="home-text">
+        <h2>Hey there! What may I call you ?</h2>
+        <input
+          type="text"
+          onChange={(e) => handleChange(e)}
+          onKeyPress={(e) => handleKeyPress(e)}
+        />
+
+        {user.length > 0 && (
+          <button
+            onClick={() => {
+              localStorage.setItem("name", user);
+              window.location.reload(false);
+            }}
+          >
+            Continue
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
